f7455921a62b1eddb275dc4659b8374f38f871e7
KK <christoph.kuhnke@exasol.com>
2023-01-27 09:23:19 +0100

    Show name of executable

--- src/test/java/com/exasol/bucketfs/native_image/NativeImageTest.java
+++ src/test/java/com/exasol/bucketfs/native_image/NativeImageTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.startsWith;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -13,8 +14,11 @@ import com.exasol.bucketfs.JarExecutor;
 @Tag("native-image")
 class NativeImageTest {
 
+    private static final Logger LOGGER = Logger.getLogger(NativeImageTest.class.getName());
+
     @Test
     void testNativeImage() throws IOException, InterruptedException {
+        LOGGER.fine("System property native.binary=" + System.getProperty("native.binary"));
         final JarExecutor executor = new JarExecutor().run("--help");
         executor.assertProcessFinishes();
         assertThat(executor.getStdOut(), startsWith("Usage: "));
