133b622df1f94a1118f65ef7c62b32f2bff80fff
KK <christoph.kuhnke@exasol.com>
2023-01-30 11:28:08 +0100

    fixed review findings

--- src/test/java/com/exasol/bucketfs/native_image/NativeImageTest.java
+++ src/test/java/com/exasol/bucketfs/native_image/NativeImageTest.java
@@ -4,22 +4,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.startsWith;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import com.exasol.bucketfs.ProcessExecutor;
+import com.exasol.bucketfs.url.OsCheck;
+import com.exasol.bucketfs.url.OsCheck.OSType;
 
 @Tag("native-image")
 class NativeImageTest {
-
-    private static final Logger LOGGER = Logger.getLogger(NativeImageTest.class.getName());
-
     @Test
     void testNativeImage() throws IOException, InterruptedException {
-        final String binary = System.getProperty("native.binary");
-        final ProcessExecutor executor = new ProcessExecutor(binary).run("--help");
+        final String suffix = new OsCheck().getOperatingSystemType() == OSType.WINDOWS ? ".exe" : "";
+        final ProcessExecutor executor = new ProcessExecutor("bfsc" + suffix).run("--help");
         executor.assertProcessFinishes();
         assertThat(executor.getStdOut(), startsWith("Usage: "));
     }
